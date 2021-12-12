0fd43a90787179e33d33dc52e2f2b687eb73554e
AlvBarros <alv.barrosc@hotmail.com>
2021-12-11 22:40:45 -0300

    day-3

--- day-3/index.js
+++ day-3/index.js
@@ -10,7 +10,7 @@ function getZeroedArray(length) {
 
 function convertBinaryInDecimal(binary) {
     let result = 0, chars = binary.split('').reverse();
-    for (let i=chars.length-1; i >= 0; i--) {
+    for (let i = chars.length - 1; i >= 0; i--) {
         if (chars[i] == "1") {
             result += Math.pow(2, i);
         }
@@ -52,4 +52,34 @@ function partOne() {
     return convertBinaryInDecimal(gammaRateInBinary) * convertBinaryInDecimal(epsilionRateInBinary);
 }
 
+function partTwo() {
+    let oxygenGeneratorRating = getFromFrequencyEveryPlace(input, true);
+    let co2ScrubberRating = getFromFrequencyEveryPlace(input, false);
+    return oxygenGeneratorRating * co2ScrubberRating;
+}
+
+function getFromFrequencyEveryPlace(totalList, mostCommon) {
+    var bitLength = totalList[0].length; // assuming they all have the same length
+    let currentList = totalList;
+    for (let place = 0; place <= bitLength; place++) {
+        if (currentList.length === 1) {
+            return convertBinaryInDecimal(currentList[0]);
+        }
+        // find the most common bit for the specific place
+        let amountOfOnes = 0, hasOneAtPlace = []; hasZeroAtPlace = [];
+        for (let i = 0; i < currentList.length; i++) {
+            let currentValue = currentList[i];
+            if (currentValue.split('')[place] === "1") {
+                amountOfOnes++;
+                hasOneAtPlace.push(currentValue);
+            } else {
+                hasZeroAtPlace.push(currentValue);
+            }
+        }
+        
+        currentList = (amountOfOnes >= currentList.length / 2) == mostCommon ? hasOneAtPlace : hasZeroAtPlace;
+    }
+}
+
 console.log(`Part One: ${partOne()}`);
+console.log(`Part Two: ${partTwo()}`);
